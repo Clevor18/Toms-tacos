c942623b6ba09af9140f39b47326c7f98c0d4240
Clevor18 <trev4coleman@gmail.com>
2016-03-08 13:56:26 -0800

    First push

---
+++ js/app.js
@@ -0,0 +1,20 @@
+/* Use the Module Pattern to create our app */
+
+var webvr = (function () {
+
+  // default app Id.
+  var id = 0;
+
+  /**
+   * @method init
+   * @description initialize the application's JS.
+   */
+  function init () {
+
+  };
+
+  return {
+    init: init
+  }; // end of returned object.
+
+})(); // immediate execution.
